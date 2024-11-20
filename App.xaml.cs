07854cfb2c2bc3e8ad419ee436ecaac355949449
meli1230 <melisa_marian@yahoo.fr>
2024-11-20 19:18:28 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MarianMelisaLab7
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
