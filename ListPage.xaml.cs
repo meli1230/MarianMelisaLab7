74afab482c5102194d5114e435993bc0e7ddab9a
meli1230 <melisa_marian@yahoo.fr>
2024-12-11 20:30:48 +0200

    laboratory10 done

--- ListPage.xaml.cs
+++ ListPage.xaml.cs
@@ -12,7 +12,9 @@ public partial class ListPage : ContentPage
 	{
 		var slist = (ShopList)BindingContext;
 		slist.Date = DateTime.UtcNow;
-		await App.Database.SaveShopListAsync(slist);
+        Shop selectedShop = (ShopPicker.SelectedItem as Shop); 
+		slist.ShopID = selectedShop.ID;
+        await App.Database.SaveShopListAsync(slist);
 		await Navigation.PopAsync();
 	}
 	async void OnDeleteButtonClicked (object sender, EventArgs e)
@@ -47,8 +49,12 @@ public partial class ListPage : ContentPage
     }
     protected override async void OnAppearing() 
 	{ 
-		base.OnAppearing(); 
-		var shopl = (ShopList)BindingContext; 
+		base.OnAppearing();
+        var items = await App.Database.GetShopsAsync(); 
+		ShopPicker.ItemsSource = (System.Collections.IList)items; 
+		ShopPicker.ItemDisplayBinding = new Binding("ShopDetails");
+
+        var shopl = (ShopList)BindingContext; 
 		listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID); 
 	}
 }
